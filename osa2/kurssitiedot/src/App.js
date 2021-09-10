cd441c4378fd3150029e9d86947db72451e27410
Arttu Linkinen <arttu.linkinen@gmail.com>
2021-09-10 18:14:13 +0300

    erillinen moduuli lisatty

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,27 +1,5 @@
 import React from 'react'
-
-const Header = ({ name }) => <h2>{name}</h2>
-
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
-
-const Content = ({ parts }) => {
-  const total = parts.reduce( ( sum, part ) => sum + part.exercises ,0)
-  return (
-    <div>
-      {parts.map(part => <Part key = {part.id} part = {part} />)}
-      <b>total of {total} exercises</b>
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name = {course.name} />
-      <Content parts = {course.parts} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const Courses = ({ courses }) => {
   return (
@@ -32,10 +10,7 @@ const Courses = ({ courses }) => {
   )
 }
 
-
-
 const App = () => {
-
 
   const courses = [
     {
