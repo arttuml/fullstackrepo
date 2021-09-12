d03f955458fb57ef0ae80e640827cf3c247ce002
Arttu Linkinen <arttu.linkinen@gmail.com>
2021-09-12 18:59:52 +0300

    puhelinluettelo step1-4

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+
+
+const Person = ({person}) => (
+  <p>{person.name} {person.number}</p>
+)
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
+  
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
+  }
+
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    if (persons.some(person => person.name === newName)){
+      window.alert(`${newName} is already added to phonebook`)
+      console.log({})
+    } else {
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+        <div>filter shown with <input value={newFilter} onChange={handleFilterChange}/></div>
+      <h2>add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {personsToShow.map(person => <Person key={person.name} person={person}/>)}
+      </div>
+    </div>
+  )
+
+}
+
+export default App
