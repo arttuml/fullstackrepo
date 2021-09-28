500e6459d18b40cdbe0a0446ba4e44319a9843a8
Arttu Linkinen <arttu.linkinen@gmail.com>
2021-09-28 15:20:19 +0300

    apufunktiota ja yksikkotesteja step3

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,25 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+  return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const favBlog = blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current)
+  return {
+    title: favBlog.title,
+    author: favBlog.author,
+    likes: favBlog.likes
+  }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
