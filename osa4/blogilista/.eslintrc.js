dafea65bde28ead708fba6a30ea879fd4912e465
Arttu Linkinen <arttu.linkinen@gmail.com>
2021-09-28 12:01:22 +0300

    eslintrc updated

--- osa4/blogilista/.eslintrc.js
+++ osa4/blogilista/.eslintrc.js
@@ -1,29 +1,31 @@
+   
 module.exports = {
-    'env': {
-        'commonjs': true,
-        'es2020': true,
-        'node': true
-    },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'ecmaVersion': 12
-    },
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
-    }
+  'env': {
+    'commonjs': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 12
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'no-console': 0,
+  }
 }
