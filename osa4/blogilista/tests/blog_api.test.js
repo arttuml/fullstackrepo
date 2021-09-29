0bb6e338d2664c48454a3fb2dd860eaee4f2b025
Arttu Linkinen <arttu.linkinen@gmail.com>
2021-09-29 16:28:44 +0300

    blogilistan laajennus step1-2 +testit

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -80,6 +80,44 @@ test('POST blog without title and url response 400 Bad request', async () => {
     .expect(400)
 })
 
+test('a note can be delete', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogsToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogsToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+
+  const titles = blogsAtEnd.map(b => b.title)
+
+  expect(titles).not.toContain(blogsToDelete.title)
+})
+
+test('a note can be updated', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogsToUpdate = blogsAtStart[0]
+
+  const update = {
+    title: 'uusi nimi blogi',
+    likes: 1000
+  }
+
+  await api
+    .put(`/api/blogs/${blogsToUpdate.id}`)
+    .send(update)
+    .expect(200)
+  
+  const blogsAtEnd = await helper.blogsInDb()
+  const updatedBlog = blogsAtEnd[0]
+  
+  expect(update.title).toEqual(updatedBlog.title)
+  expect(update.likes).toEqual(updatedBlog.likes) 
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
