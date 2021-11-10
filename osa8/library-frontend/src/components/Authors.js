6bfd59267fb741b5cfc0a45bc3ba89124cea6eb8
Arttu Linkinen <arttu.linkinen@gmail.com>
2021-11-10 15:26:16 +0200

    8.8-8.12 library-fronend

---
+++ osa8/library-frontend/src/components/Authors.js
@@ -0,0 +1,79 @@
+import { useMutation, useQuery } from '@apollo/client'
+import React, { useState } from 'react'
+import { ALL_AUTHORS, EDIT_BORN } from '../queries'
+
+const Authors = (props) => {
+  const [name, setName] = useState('Robert Martin')
+  const [born, setBorn] = useState('')
+
+  const result = useQuery(ALL_AUTHORS)
+  
+  const [ addBorn ] = useMutation(EDIT_BORN, {
+    refetchQueries: [{ query: ALL_AUTHORS }]
+  })
+
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
+  
+  if (!props.show) {
+    return null
+  }
+
+  const authors = result.data.allAuthors
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    addBorn({ variables: { name, setBornTo: born }})
+    
+    setName('Robert Martin')
+    setBorn('')
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <h2>Set birthyear</h2>
+      <form onSubmit={submit}>
+        <label>
+          name
+          <select value={name} onChange={({ target }) => setName(target.value)}>
+            {authors.map(a => <option value={a.name}>{a.name}</option> )}
+          </select>
+        </label>
+        <div>
+          born <input
+            type='number'
+            value={born}
+            onChange={({target}) => setBorn(Number(target.value))}
+          />
+        </div>
+        <button type='submit'>update author</button>
+      </form>
+
+    </div>
+  )
+}
+
+export default Authors
